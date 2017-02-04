aa2563a76fd3a9b0d29803070843e8388e0ff62c
linzr <noctislin@gmail.com>
2017-02-05 01:10:41 +0800

    Test 4 exc

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -18,7 +18,8 @@ class StudentController extends Controller
     } 
 
     public function detail($id) {
-        require_once '/var/www/html/cs3226/vendor/fzaninotto/faker/src/autoload.php';
+        require_once 'vendor/autoload.php';
+
 
         $faker = Faker\Factory::create();
 

2cb2c3505019da7d4d724442aa96ad9ed896c65d
linzr <noctislin@gmail.com>
2017-02-05 04:14:39 +0800

    Update detail view 3.

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,7 @@
 @section('main')
      <div class="container-fluid">
         <div class="row">
-          <div class="col-md-6 col-xs-12" >
+          <div class="col-md-8 col-xs-12" >
             <header>
               <h3><strong>{{ $student->name }}</strong> <a href="#"><img src="../img/kattis.png" alt="Kattis" width="20" height="15"></a> in CS3233 S1 AY 2020/21</h3>
               <p><strong>SPE</strong>(ed) component: <strong>{{ $student->mc }} + {{ $student->tc }} = {{ $student->mc+$student->tc }}</strong><br>
@@ -13,7 +13,7 @@
               <strong>Sum = SPE + DIL = {{ $student->mc+$student->tc }} + {{ $student->hw+$student->bs+$student->ks+$student->ac }} = {{ $student->mc+$student->tc+$student->hw+$student->bs+$student->ks+$student->ac }}</strong></p>
             </header>
           </div>
-          <div class="col-md-6 hidden-xs hidden-sm">
+          <div class="col-md-4 hidden-xs hidden-sm">
             <img class="pull-right" id="photo" src="../img/locked.png" alt="Photo of {{ $student->name }}" width="96" height="150">
             <img class="pull-right" id="flag" src="../flags/4x3/{{strtolower($student->country)}}.svg" alt="{{$student->country}} Flag" width="100">
           </div>

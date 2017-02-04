796bba3228fc06eb42aa9fb295125d394bbc8254
linzr <noctislin@gmail.com>
2017-02-05 02:18:32 +0800

    Update seed.

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -11,6 +11,20 @@ class StudentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        $limit = 50;
+
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('customers')->insert([ //,
+                'name'=>$faker->unique()->name,
+                'mc' => $faker->randomDigit,
+                'tc' => $faker->randomDigit,
+                'hw' => $faker->randomDigit,
+                'bs' => $faker->randomDigit,
+                'ks' => $faker->randomDigit,
+                'ac' => $faker->randomDigit,
+            ]);
+        }
     }
 }

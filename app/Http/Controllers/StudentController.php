0e28e413cc52297b4047ffce938a5d0cb02414ba
linzr <noctislin@gmail.com>
2017-02-05 02:41:24 +0800

    Update detail view.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -19,6 +19,16 @@ class StudentController extends Controller
     public function detail($id) {
         $user = DB::table('student')->where('id', $id)->first();
 
-        return view('detail',['id' => $user->id,'name' => $user->name, 'mc' => $user->mc,'tc' => $user->tc,'hw' => $user->hw,'bs' => $user->bs,'ks' => $user->ks,'ac' => $user->ac]);
+        $mc = $user->mc;
+        $tc = $user->tc;
+        $hw = $user->hw;
+        $bs = $user->bs;
+        $ks = $user->ks;
+        $ac = $user->ac;
+        $spe= $mc + $tc;
+        $dil= $hw + $bs + $ks + $ac;
+        $sum= $spe + $dil;
+
+        return view('detail',['id' => $user->id,'name' => $user->name, 'mc' => $mc,'tc' => $tc,'hw' => $hw,'bs' => $bs,'ks' => $ks,'ac' => $ac, 'spe'=> $spe, 'dil'=>$dil, 'sum'=>$sum]);
     }
 }
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,13 +1,23 @@
 @extends('template')
 @section('title')
-    Student {{ $name }}
+    {{ $name }} - Student Detail
 @endsection
 @section('main')
-    <div class="container-fluid">
+     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12" >
-                <p>This page will show the details of student with this name {{ $name }} and id {{ $id }}.</p>
-            </div>
+          <div class="col-md-6 col-xs-12" >
+            <header>
+              <h3><strong>Delano Edwards</strong> in CS3233 S1 AY 2020/21</h3>
+              <p><a href="#">Kattis account <img src="../img/kattis.png" alt="Kattis" width="20" height="15"></a></p>
+              <p><strong>SPE</strong>(ed) component: <strong>{{ $mc }} + {{ $tc }} = {{ $spe }}</strong><br>
+              <strong>DIL</strong>(igence) component: <strong>{{ $hw }} + {{ $bs }} + {{ $ks }} + {{ $ac }} = {{ $dil }}</strong><br>
+              <strong>Sum = SPE + DIL = {{ $spe }} + {{ $dil }} = {{ $sum }}</strong></p>
+            </header>
+          </div>
+          <div class="col-md-6 hidden-xs hidden-sm">
+            <img class="pull-right" id="photo" src="../img/locked.png" alt="Photo of {{ $name }}" width="96" height="150">
+            <img class="pull-right" id="flag" src="../img/CHN.png" alt="China Flag" width="100">
+          </div>
         </div>
     </div>
 @endsection

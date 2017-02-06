b84afee9dd81fe4d7857f2fa086ba7fd5cee005e
chengim <chengim@users.noreply.github.com>
2017-02-06 15:52:22 +0800

    Update radarChart.blade.php

--- resources/views/radarChart.blade.php
+++ resources/views/radarChart.blade.php
@@ -1,5 +1,7 @@
 @section('radarChart')
   <div class="radarChart"></div>
+@endsection
 
+@section('footer')
   <script type="text/javascript" src="../../public/js/radarChart.js"></script>
 @endsection

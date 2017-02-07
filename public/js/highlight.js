7c4b1dfcfdbd895e5cb9eb0222601c58c57b65b8
chengim <chengim.sg@gmail.com>
2017-02-07 12:58:58 +0800

    test page indicator

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -84,3 +84,9 @@ function setAllDifferentHeight()
 
     }    
 }
+
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+})

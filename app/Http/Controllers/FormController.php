4dc23bb73e1eec329e228a3d86d24f82fa09383f
chengim <chengim.sg@gmail.com>
2017-02-12 01:44:12 +0800

    test 13

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,20 +9,13 @@ use Validator;
 
 class FormController extends Controller {
   public function test() { return view('test'); }
-
-  public function check(Request $request) {
-    // ps, yeah, I 'cheat' by combining Controller and View together...
-    // separate them in your actual project
-    $name = $request->input('name');
-    $ans = $request->input('mcq');
-    return "<p>" . $name . ", you selected: " . $ans . "<br>" . 
-           "Correct: " . ($ans == 10 ? "Y" : "N") . "<br></p>";
-  }
   
   public function check(Request $request) {
     $validator = Validator::make($request->all(), [ // as simple as this
-      'name' => 'required|min:3|max:10',
-      'mcq' => 'required',
+      'nickname' => 'required|min:5|max:30',
+      'fullname' => 'required|min:5|max:30',
+	  'kattisacct' => 'required|min:5|max:30',
+	  'nationality' => 'required',
     ]);
     if ($validator->fails()) {
       return redirect('test') // redisplay the form

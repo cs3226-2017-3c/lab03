95e80978077a7c1b743ec2e698902f81823f63f8
chengim <chengim.sg@gmail.com>
2017-02-12 01:50:05 +0800

    test 14

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,15 +11,12 @@ class FormController extends Controller {
   public function test() { return view('test'); }
   
   public function check(Request $request) {
-    $validator = Validator::make($request->all(), [ // as simple as this
+    Validator::make($request->all(), [ // as simple as this
       'nickname' => 'required|min:5|max:30',
       'fullname' => 'required|min:5|max:30',
 	  'kattisacct' => 'required|min:5|max:30',
 	  'nationality' => 'required',
-    ]);
-    if ($validator->fails()) {
-      return redirect('test') // redisplay the form
-             ->withErrors($validator) // to see the error messages
-             ->withInput(); // the previously entered input remains
+    ])->validate();
+
   }
 }

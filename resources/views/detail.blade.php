e800ef68177d865268c1056762692dc3939447f6
linzr <noctislin@gmail.com>
2017-02-12 03:44:22 +0800

    Update detail view.

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -11,15 +11,21 @@
         <p><strong>SPE</strong>(ed) component: <strong>{{ $student->mc }} + {{ $student->tc }} = {{ $student->mc+$student->tc }}</strong><br>
           <strong>DIL</strong>(igence) component: <strong>{{ $student->hw }} + {{ $student->bs }} + {{ $student->ks }} + {{ $student->ac }} = {{ $student->hw+$student->bs+$student->ks+$student->ac }}</strong><br>
           <strong>Sum = SPE + DIL = {{ $student->mc+$student->tc }} + {{ $student->hw+$student->bs+$student->ks+$student->ac }} = {{ $student->mc+$student->tc+$student->hw+$student->bs+$student->ks+$student->ac }}</strong></p>
-        </header>
-      </div>
-      <div class="col-md-4 hidden-xs hidden-sm">
-        <img class="pull-right" id="photo" src="@if($student->avatar) {{Storage::url($student->avatar)}} @else ../img/locked.png @endif" alt="Photo of {{ $student->name }}" width="100" height="100">
-        <img class="pull-right" id="flag" src="../flags/4x3/{{strtolower($student->country)}}.svg" alt="{{$student->country}} Flag" width="100">
-        <canvas id="myChart" width="350" height="350"></canvas>
-      </div>
+      </header>
+    </div>
+    <div class="col-md-4 hidden-xs hidden-sm">
+      <img class="pull-right" id="photo" src="@if($student->avatar) {{Storage::url($student->avatar)}} @else ../img/locked.png @endif" alt="Photo of {{ $student->name }}" width="100" height="100">
+      <img class="pull-right" id="flag" src="../flags/4x3/{{strtolower($student->country)}}.svg" alt="{{$student->country}} Flag" width="100">
     </div>
   </div>
+  <div class="row">
+    <div class="col-md-8 col-xs-12" >
+    </div>
+    <div class="col-md-4 hidden-xs hidden-sm">
+      <canvas id="myChart" width="350" height="350"></canvas>
+    </div>    
+  </div>
+</div>
   @endsection
   @section('footer')
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>

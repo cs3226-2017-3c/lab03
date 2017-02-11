067a315050739c8e85b2e6e57bf869fe3581c7f5
linzr <noctislin@gmail.com>
2017-02-12 02:15:32 +0800

    Update js.

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -18,12 +18,13 @@ function setAllDifferentHeight()
 {
     setTimeout(function() {
         var t = document.getElementById("myTable");
+        t.rows[1].style.height = "30px";
         for(var i=2; i < t.rows.length; i++) {
             var sumi = t.rows[i].cells[11].innerHTML;
             var sumiminus1 = t.rows[i-1].cells[11].innerHTML;
             t.rows[i].style.height = 30+30*(Math.abs(sumi-sumiminus1)).toString()+"px";
 
         }   
-    }, 1000);
+    }, 100);
 }
 

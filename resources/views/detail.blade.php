acbd21255135c932447b5c0cb259ddb7d0fe8a31
linzr <noctislin@gmail.com>
2017-02-12 03:27:52 +0800

    Test again.

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -22,7 +22,7 @@
   </div>
   @endsection
   @section('footer')
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.js"></script>
   <script>
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {

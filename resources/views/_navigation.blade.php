e4186434b0d8e18761f692572fef895488250223
linzr <noctislin@gmail.com>
2017-02-11 12:28:08 +0800

    Nav bar 2

--- resources/views/_navigation.blade.php
+++ resources/views/_navigation.blade.php
@@ -12,7 +12,7 @@
   		</div>
   		<div id="navbar" class="navbar-collapse collapse">
   			<ul class="nav navbar-nav">
-  				<li id="home" {{ Request::path() == "/" ? 'class="active"' : '' }}"><a href="/">Home</a></li>
+  				<li id="home" {{ Request::path() == "" ? 'class="active"' : '' }}"><a href="/">Home</a></li>
   				<li id="help" {{ Request::is('help') ? 'class="active"' : '' }}><a href="/help">Help</a></li>
           {{ Request::is('student/create') ? '<li id="Create Student Mode" class="active"><a href="#">Create Student Mode</a></li>' : '' }}
           {{ Request::is('*/edit') ? '<li id="Edit Mode" class="active"><a href="#">Edit Mode</a></li>' : '' }}

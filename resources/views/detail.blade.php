457816d5ffe8ef4930d1b8fa1c460d6a11fdecc8
Sahand Zarrinkoub <sahandz@kth.se>
2017-02-12 02:01:50 +0800

    -

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -30,39 +30,9 @@
   var ctx = document.getElementById("myChart");
   var myChart = new Chart(ctx, {
       type: 'radar',
-      data: {
-          labels: ["MC", "TC", "HW", "Bs", "KS", "Ac"],
-          datasets: [{
-              label: '# of Votes',
-              data: [$student->mc,$student->tc,$student->hw,$student->bs,$student->ks,$student->ac],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
-      }
+      labels: ["MC", "TC", "HW", "Bs", "KS", "Ac"],
+      data: [$student->mc,$student->tc,$student->hw,$student->bs,$student->ks,$student->ac]
+
   });
   </script>
 @endsection

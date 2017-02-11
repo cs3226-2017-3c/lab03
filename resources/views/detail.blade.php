94a6a4eb1a4f896b17762044401f0fbc70a78403
Sahand Zarrinkoub <sahandz@kth.se>
2017-02-12 01:58:48 +0800

    radarchart

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,9 +1,12 @@
+
 @extends('template')
+@section('header')
+<script type="text/javascript" src="js/Chart.js"></script>
+@endsection
 @section('title')
 {{ $student->name }} - Student Detail
 @endsection
 @section('main')
-@yield('radarChart')
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-8 col-xs-12" >
@@ -20,4 +23,46 @@
       </div>
     </div>
   </div>
+  <canvas id="myChart" width="400" height="400"></canvas>
   @endsection
+@section('footer')
+  <script>
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'radar',
+      data: {
+          labels: ["MC", "TC", "HW", "Bs", "KS", "Ac"],
+          datasets: [{
+              label: '# of Votes',
+              data: [$student->mc,$student->tc,$student->hw,$student->bs,$student->ks,$student->ac],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+  </script>
+@endsection

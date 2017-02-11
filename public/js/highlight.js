46859cd4a0f20d86822b4a60107ed3b8e63d43b4
linzr <noctislin@gmail.com>
2017-02-11 12:53:11 +0800

    Update JS.

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -1,22 +1,15 @@
 $(document).ready(function() 
 { 
     $("#myTable").tablesorter(); 
+    setAllDifferentHeight();
 } 
 ); 
 
-
-$(document).ready(function () {
-    //Setting row height according to difference between SUMs
-
-    setAllDifferentHeight();
-}); 
-
-
 function setAllSameHeight()
 {
-    tablerows = document.getElementById("myTable").rows;
-    for(i=0;i<tablerows.length;i++){
-        tablerows[i].style.height = "30px"; 
+    var t = document.getElementById("myTable");
+    for(var i=0; i<t.rows.length; i++){
+        t.rows[i].style.height = "30px"; 
     }
     
 }
@@ -24,7 +17,6 @@ function setAllSameHeight()
 function setAllDifferentHeight()
 {
     var t = document.getElementById("myTable");
-
     for(var i=2; i < t.rows.length; i++) {
         var sumi = t.rows[i].cells[11].innerHTML;
         var sumiminus1 = t.rows[i-1].cells[11].innerHTML;

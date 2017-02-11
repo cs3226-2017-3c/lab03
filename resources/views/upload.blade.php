711ed0c9ac417e33c9765c0affb7ed5d3585a215
linzr <noctislin@gmail.com>
2017-02-11 13:24:04 +0800

    Update upload view.

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -6,20 +6,13 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12" >
-      <header>
-        {!! Form::open(['url' => 'student/upload']) !!}
-        <div class="form-group"> {{-- Group related form components together --}}
-          {!! Form::label('name', 'What is your name:', ['class' => 'control-label']) !!}
-          {!! Form::text('name', null, ['class' => 'form-control']) !!}
-        </div>
+        <h2>Upload image for <strong>{{ $student->name }}</strong></h2>
+        {!! Form::open(['url' => 'StudentController@upload_image', 'files' => 'true']) !!}
         <div class="form-group">
-          {!! Form::label('mcq', 'What is your answer:', ['class' => 'control-label']) !!}
-          {!! Form::radio('mcq', '9', false, ['class' => 'form-control']) !!}A.9
-          {!! Form::radio('mcq', '10', false, ['class' => 'form-control']) !!}B.10
-          {!! Form::radio('mcq', '11', false, ['class' => 'form-control']) !!}C.11
+          {!! Form::file('image') !!}
         </div>
-        <div class="form-group"> {{-- Don't forget to create a submit button --}}
-          {!! Form::submit('Submit', ['class' => 'form-control']) !!}
+        <div class="form-group"> 
+          {!! Form::submit('Submit', ['class' => 'btn btn-primary btn-lg']) !!}
         </div>
         {!! Form::close() !!}
       </div>

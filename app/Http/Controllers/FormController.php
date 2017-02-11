5e59c075829e414119a1a6567f208e406700be3a
chengim <chengim.sg@gmail.com>
2017-02-11 22:59:24 +0800

    test 5

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -10,14 +10,7 @@ use Validator;
 class FormController extends Controller {
   public function test() { return view('test'); }
 
-  public function check(Request $request) {
-    // ps, yeah, I 'cheat' by combining Controller and View together...
-    // separate them in your actual project
-    $name = $request->input('name');
-    $ans = $request->input('mcq');
-    return "<p>" . $name . ", you selected: " . $ans . "<br>" . 
-           "Correct: " . ($ans == 10 ? "Y" : "N") . "<br></p>";
-  }
+
   
    public function check(Request $request) {
     $validator = Validator::make($request->all(), [ // as simple as this

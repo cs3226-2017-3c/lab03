c2646959df6fcffd1e1450d2ae6c312ed93257ea
linzr <noctislin@gmail.com>
2017-02-12 14:41:45 +0800

    Fix index controller.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,11 +14,21 @@ class StudentController extends Controller
      * @return Response
      */
     public function index() { 
-        $students = DB::table('student')->get()->sortByDesc(function ($a, $key) {
+        $students = DB::table('student')->get()->map(function ($value, $key) {
+            $arr = array('mc','tc','hw','bs','ks','ac');
+            if(in_array($key,$arr)){
+                return this->sum_score($value);
+            }
+        })->sortByDesc(function ($a, $key) {
             return  $a->mc+$a->tc+$a->hw+$a->bs+$a->ks+$a->ac ;
         });
         return view('index', ['student' => $students]); 
     } 
+
+    private function sum_score($score){
+        $arr = explode(",", $score);
+        return array_sum($arr);
+    }
 
     public function detail($id) {
         $leader = DB::table('student')->get()->sortByDesc(function ($a, $key) {

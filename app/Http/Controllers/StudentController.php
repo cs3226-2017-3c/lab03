f4809797a922c7ea2bede95bc93fc90238f24b8d
linzr <noctislin@gmail.com>
2017-02-12 14:57:06 +0800

    Update.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -17,7 +17,7 @@ class StudentController extends Controller
         $students = DB::table('student')->get()->map(function ($value, $key) {
             $arr = array('mc','tc','hw','bs','ks','ac');
             foreach($arr as $column){
-                $value->get($column) = sum_score($value->get($column));
+                $value->put($column, sum_score($value->get($column)));
             }
             return $value;
         })->sortByDesc(function ($a, $key) {
